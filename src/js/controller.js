96b33263dbe485f242df04bdec58a72b1355022d
Aleksandar Milic <alexmilic01@gmail.com>
2021-08-17 22:51:06 +0200

    Add fake links

--- src/js/controller.js
+++ src/js/controller.js
@@ -32,9 +32,13 @@ const renderSpinner = function(parentEl) {
 
 const showRecipe = async function() {
     try {
+        const id = window.location.hash.slice(1);
+
+        if(!id) return;
+        
         renderSpinner(recipeContainer);
         const res = await fetch(
-            'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+            `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
         );
         const data = await res.json();
 
@@ -160,3 +164,5 @@ const showRecipe = async function() {
 }
 
 showRecipe();
+window.addEventListener('load', showRecipe);
+window.addEventListener('hashchange', showRecipe);
