09e58982eb0529a726cdf00b623951c589c7f539
Aleksandar Milic <alexmilic01@gmail.com>
2021-08-23 21:02:37 +0200

    Add helpers funcions

--- src/js/model.js
+++ src/js/model.js
@@ -1,3 +1,7 @@
+import { async } from 'regenerator-runtime';
+import { API_URL } from './config.js';
+import { getJSON } from './helpers.js';
+
 export const state = {
     recipe: {}
 }
@@ -5,12 +9,7 @@ export const state = {
 export const loadRecipe = async function(id) {
     try {
 
-        const res = await fetch(
-            `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-        );
-        const data = await res.json();
-
-        if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+        const data = await getJSON(`${API_URL}/${id}`);
 
         const recipe = data.data.recipe;
         // const {recipe} = data.data;
@@ -25,8 +24,7 @@ export const loadRecipe = async function(id) {
             ingredients: recipe.ingredients
         }
 
-        console.log(state.recipe);
     } catch(error) {
-        alert(error);
+       console.error(error);
     }
 }
