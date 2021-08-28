502e948ab77243bb75b8fe973100ea6cfb32a5dd
Aleksandar Milic <alexmilic01@gmail.com>
2021-08-28 15:42:29 +0200

    Update servings

--- src/js/controller.js
+++ src/js/controller.js
@@ -24,7 +24,6 @@ const controlRecipes = async function() {
 
         // 2) render recipe
         recipeView.render(model.state.recipe);
-
     } catch (error) {
        recipeView.renderErorr();
     }
@@ -59,8 +58,17 @@ const controlPagination = function(goToPage) {
     paginationView.render(model.state.search);
 }
 
+const controlServings = function(newServings) {
+    // Update the recipe servings (in state)
+    model.updateServings(newServings);
+
+    // Update the recipe view
+    recipeView.render(model.state.recipe);
+}
+
 const init = function() {
     recipeView.addHandlerRender(controlRecipes);
+    recipeView.addHandlerUpdateServings(controlServings);
     searchView.addHandlerSearch(controlSearchResults);
     paginationView.addHandlerClick(controlPagination);
 }
