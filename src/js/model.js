486fef0e6b538ec414b53c92e165839e5d5b15cc
Aleksandar Milic <alexmilic01@gmail.com>
2021-08-28 17:48:21 +0200

    Start bookmarks functionallity

--- src/js/model.js
+++ src/js/model.js
@@ -9,7 +9,8 @@ export const state = {
         results: [],
         page: 1,
         resultsPerPage: RES_PER_PAGE
-    }
+    },
+    bookmarks: []
 }
 
 export const loadRecipe = async function(id) {
@@ -28,6 +29,12 @@ export const loadRecipe = async function(id) {
             servings: recipe.servings,
             cookingTime: recipe.cooking_time,
             ingredients: recipe.ingredients
+        };
+
+        if(state.bookmarks.some(bookmark => bookmark.id === id)) {
+            state.recipe.bookmarked = true;
+        } else {
+            state.recipe.bookmarked = false;
         }
 
     } catch(error) {
@@ -48,7 +55,8 @@ export const loadSearchResults = async function(query) {
                 publisher: recipe.publisher,
                 image: recipe.image_url
             }
-        })
+        });
+        state.search.page = 1;
     } catch(error) {
         console.error(`${error}`);
         throw error;
@@ -69,4 +77,24 @@ export const updateServings = function(newServings) {
     });
 
     state.recipe.servings = newServings;
+}
+
+export const addBookmark = function(recipe) {
+    // Add bookmark
+    state.bookmarks.push(recipe);
+
+    // Mark current recipe as bookmark
+    if(recipe.id === state.recipe.id) {
+        state.recipe.bookmarked = true;
+    }
+}
+
+export const deleteBookmark = function(id) {
+    // delete bookmark
+    const index = state.bookmarks.findIndex(el => el.id === id);
+    state.bookmarks.splice(index, 1);
+    // Mark current recipe as NOT bookmarked
+    if(id === state.recipe.id) {
+        state.recipe.bookmarked = false;
+    }
 }
