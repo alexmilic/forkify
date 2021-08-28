2e50acb11444ac0acd74631b884fd2b4079e90cb
Aleksandar Milic <alexmilic01@gmail.com>
2021-08-28 19:08:24 +0200

    Bookmark logic

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,6 +3,7 @@ import recipeView from './ views/recipeView.js';
 import searchView from './ views/searchView.js';
 import resultsView from './ views/resultsView.js';
 import paginationView from './ views/paginationView.js';
+import bookmarksView from './ views/bookmarksView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -20,7 +21,8 @@ const controlRecipes = async function() {
         recipeView.renderSpinner();
 
         // update results view to mark selected result
-        resultsView.update(model.getSearchResultsPage() );
+        resultsView.update(model.getSearchResultsPage());
+        bookmarksView.update(model.state.bookmarks);
 
         // 1) load recipe
         await model.loadRecipe(id);
@@ -71,14 +73,19 @@ const controlServings = function(newServings) {
 }
 
 const controlAddBookmark = function() {
+    // Add/remove bookmark
     if(!model.state.recipe.bookmarked) {
         model.addBookmark(model.state.recipe);
     } else {
         model.deleteBookmark(model.state.recipe.id);
     }
 
+    // Update recipe view
     recipeView.update(model.state.recipe);
-    console.log(model.state.recipe);
+    // console.log(model.state.recipe);
+
+    // Render bookmarks
+    bookmarksView.render(model.state.bookmarks);
 }
 
 const init = function() {
